4dbedf98f889fe00c0418710b410ffa91e44dc7a
kazu-S-1110 <xtpgdmw3690@yahoo.co.jp>
2021-07-20 12:37:45 +0900

    削除機能を追加。spliceを初めて使ったかも

--- src/App.js
+++ src/App.js
@@ -5,6 +5,16 @@ export const App = () => {
   const [inCompleteTodos, setInCompleteTodos] = useState(['todo1', 'todo2']);
   const [completeTodos, setCompleteTodos] = useState(['todo3']);
   const [inputText, setInputText] = useState('');
+  const addTodo = () => {
+    if (inputText !== '') {
+      setInCompleteTodos([...inCompleteTodos, inputText], setInputText(''));
+    }
+  };
+  const deleteTodo = (index) => {
+    const newTodos = [...inCompleteTodos];
+    newTodos.splice(index, 1);
+    setInCompleteTodos(newTodos);
+  };
 
   return (
     <>
@@ -15,29 +25,17 @@ export const App = () => {
           value={inputText}
           onChange={(e) => setInputText(e.target.value)}
         ></input>
-        <button
-          onClick={
-            inputText !== ''
-              ? () =>
-                  setInCompleteTodos(
-                    [...inCompleteTodos, inputText],
-                    setInputText('')
-                  )
-              : ''
-          }
-        >
-          Add
-        </button>
+        <button onClick={addTodo}>Add</button>
       </div>
       <div className={styles.incomplete_area}>
         <p className={styles.title}>Non-completeList</p>
         <ul>
-          {inCompleteTodos.map((todo) => {
+          {inCompleteTodos.map((todo, index) => {
             return (
               <li key={todo} className={styles.list_row}>
                 <div>{todo}</div>
                 <button>Finish</button>
-                <button>Delete</button>
+                <button onClick={() => deleteTodo(index)}>Delete</button>
               </li>
             );
           })}
@@ -50,8 +48,7 @@ export const App = () => {
             return (
               <li key={todo} className={styles.list_row}>
                 <div>{todo}</div>
-                <button>Finish</button>
-                <button>Delete</button>
+                <button>Back</button>
               </li>
             );
           })}
